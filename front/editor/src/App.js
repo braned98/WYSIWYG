840d59a23b486595d3e497633ff4dc510f3a3259
braned98 <bdobrokes@gmail.com>
2022-12-09 11:08:51 +0100

    [Feature] editor init

--- front/editor/src/App.js
+++ front/editor/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
 
 function App() {
+
+  const [editor] = useState(() => withReact(createEditor()))
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slate editor={editor} value={initialValue}>
+      <Editable />
+    </Slate>
   );
 }
 
