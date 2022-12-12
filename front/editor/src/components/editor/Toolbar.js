ac7dfa8f3b99b184039b0c493332a2b15561a2cc
braned98 <bdobrokes@gmail.com>
2022-12-12 11:44:07 +0100

    [Feature] selection control

---
+++ front/editor/src/components/editor/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Button from '../UI/Button';
+
+
+const Toolbar = (props) => {
+    return <div className='toolbar'>
+        <Button>B</Button>
+    </div>
+
+};
+
+
+export default Toolbar;
+
+
+
+
+
+
+
+
+
+
+
--- front/editor/src/components/editor/editor.js
+++ front/editor/src/components/editor/editor.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import './editor.css';
+import useSelection  from '../../hooks/use-selection';
+import Toolbar from "./Toolbar";
 
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-];
 
-function Editor() {
+
+function Editor({document, onChange}) {
   const [editor] = useState(() => withReact(createEditor()));
+
+  const [selection, setSelection] = useSelection(editor);
+
+  const onChangeHandler = useCallback(
+    (doc) => {
+        onChange(doc);
+        setSelection(editor.selection);
+    },
+    [editor.selection, onChange, setSelection]
+  );
 
   return (
     <div className="editor">
-      <Slate editor={editor} value={initialValue}>
+      <Slate editor={editor} value={document} onChange={onChangeHandler}>
+        <Toolbar selection={selection} />
         <Editable />
       </Slate>
     </div>
