902150d8ba464948e6f52c0913a4cd5a87e945ad
braned98 <bdobrokes@gmail.com>
2022-12-13 10:54:01 +0100

    [Feature] backend init & models

---
+++ back/wysiwyg/wysiwyg/Models/MyWebApiContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace wysiwyg.Models
+{
+	public class MyWebApiContext:DbContext
+	{
+        public MyWebApiContext(DbContextOptions<MyWebApiContext> options) : base(options) { }
+    }
+}
+
---
+++ back/wysiwyg/wysiwyg/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace wysiwyg.Models
+{
+	public class User
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+		public ICollection<Document> Documents { get; set; } = new List<Document>();
+
+
+		public User(string name, string username, string password, string email)
+		{
+			Name = name;
+			Username = username;
+			Password = password;
+			Email = email;
+		}
+
+	}
+}
+
